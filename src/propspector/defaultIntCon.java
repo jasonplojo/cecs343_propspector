ff333f77ebb7ae137adf78297c2113d40b452aec
erdownie <charlessteal@gmail.com>
2016-12-01 14:40:30 -0800

    Default Conditions, Room, Attic, Bathroom, Interface
    Updated Room, Attic, Bathroom, Interfaces, added enums for default
    conditions.  Completed Internal and External Conditions classes.

--- src/propspector/defaultIntCon.java
+++ src/propspector/defaultIntCon.java
@@ -1,7 +1,15 @@
 package propspector;
 	//because these are default hardcoded values, no mutators
 public enum defaultIntCon{ //For storing default interior conditions - if we decide to keep those
-	TODO("add these", false);
+	PaintPeeling("The paint is peeling.", true), //first value is user-friendly string, second is damage
+	WallsHoles("Some walls have holes in them", true),
+	CeilingDamaged("The ceiling is damaged.", true),
+	CeilingLeaking("The ceiling is leaking.", true),
+	CeilingPopcorn("The ceiling has popcorn.", false),
+	ElectricalDanger("The electrical is dangerously miswired.", true),
+	MissingOutlet("An outlet is missing.", true),
+	DamagedOutlet("An outlet is damaged.", true),
+	PlumbingLeak("The plumbing in the wall is leaking.", true);
 	
 	private String description;
 	private boolean isDamage;
